5d4203452c71579e0deed62ee09a9f72e6b08b36
kaniadhea <kaniadhea7@gmail.com>
2023-03-06 16:05:11 +0700

    first commit

---
+++ example.cpp
@@ -0,0 +1,22 @@
+//library
+#include <iostream>
+using namespace std;
+
+//program main 
+int main(){//begin 
+
+//numeric p,l,luas 
+double p,l,luas;
+//dispay "masukan panjang"
+count<<"masukan panjang =";
+//accept p 
+cin>>p;
+//displey "masukan lebar =";
+cout<<"masukan lebar ="; 
+//accept l 
+cin>>1; 
+//compute Luas=p*l
+luas=p*l
+//displey "luas persegi panjang adalah", luas 
+cout<<"luas persegi panjang adalah" 
+
